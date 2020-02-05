b686e0dc441686e2de6dd3843c53b41dadeb376e
DariusM6s <dariusm6s@outlook.com>
2020-02-05 15:44:52 +0000

    Updates

---
+++ precache-manifest.010cec411d59b0e078834c4524d7aabf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e50fa20c76e9771e03423aba3154a65",
+    "url": "/RoboFriends-React-App/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RoboFriends-React-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d4f956f07bffdefcc264",
+    "url": "/RoboFriends-React-App/static/css/main.e67de073.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RoboFriends-React-App/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RoboFriends-React-App/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f956f07bffdefcc264",
+    "url": "/RoboFriends-React-App/static/js/main.872b4053.chunk.js"
+  },
+  {
+    "revision": "d34c02a9030db2563ac8",
+    "url": "/RoboFriends-React-App/static/js/runtime-main.473a6471.js"
+  }
+]);
